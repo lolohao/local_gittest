5ce11268e0ad63635dbc72ced5558c8ee6d30d49
luohao <670133132@qq.com>
2020-05-02 16:47:19 +0800

    local git test upload pytest file

---
+++ pytest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 172.20.72.137
+dict_test = {"a":1, "c":3,"b":2}
+print(dict_test)
+for key, value in sorted(dict_test.items()):
+    print(key, value, dict_test[key])
+
